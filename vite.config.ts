0f8e779c3d3c96fc1ea615bf1f7a1032ab93e594
aryanmaske052005 <142209240+aryanmaske052005@users.noreply.github.com>
2025-09-24 11:42:34 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { createServer } from "./server";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
